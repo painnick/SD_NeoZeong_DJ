71b02a495014b9aa8b7cb8fece39631d822c86d5
painnick <painnick@gmail.com>
2023-09-19 21:48:51 +0900

    REFACTORING.

--- src/NeoPixelEffects.h
+++ src/NeoPixelEffects.h
@@ -14,17 +14,17 @@ int CurrentBright = DEFAULT_BRIGHT;
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(Adafruit_NeoPixel &strip, byte WheelPos) {
+uint32_t Wheel(byte WheelPos) {
     WheelPos = 255 - WheelPos;
     if (WheelPos < 85) {
-        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+        return Adafruit_NeoPixel::Color(255 - WheelPos * 3, 0, WheelPos * 3);
     }
     if (WheelPos < 170) {
         WheelPos -= 85;
-        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+        return Adafruit_NeoPixel::Color(0, WheelPos * 3, 255 - WheelPos * 3);
     }
     WheelPos -= 170;
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return Adafruit_NeoPixel::Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 void rainbow(Adafruit_NeoPixel &strip, uint8_t wait) {
@@ -32,7 +32,7 @@ void rainbow(Adafruit_NeoPixel &strip, uint8_t wait) {
 
     for (j = 0; j < 256; j++) {
         for (i = 0; i < strip.numPixels(); i++) {
-            strip.setPixelColor(i, Wheel(strip, (i + j) & 255));
+            strip.setPixelColor(i, Wheel((i + j) & 255));
         }
         strip.show();
         delay(wait);
@@ -45,7 +45,7 @@ void rainbowCycle(Adafruit_NeoPixel &strip, uint8_t wait) {
 
     for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
         for (i = 0; i < strip.numPixels(); i++) {
-            strip.setPixelColor(i, Wheel(strip, ((i * 256 / strip.numPixels()) + j) & 255));
+            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
         }
         strip.show();
         delay(wait);
